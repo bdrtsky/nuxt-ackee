487c2fbb2da7459a281b131b1d29ccf9771f3fe0
lihbr <lihbr@users.noreply.github.com>
2021-01-25 21:03:16 +0100

    chore: prevent code duplication in example

--- example/pages/bar.vue
+++ example/pages/bar.vue
@@ -1,40 +1,4 @@
-<template>
-  <div>
-    <pre :style="{ display: 'inline'}">/bar</pre> works!
-    <ul>
-      <li>
-        <nuxt-link to="/">
-          Index
-        </nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/foo">
-          Foo
-        </nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/bar">
-          Bar
-        </nuxt-link>
-      </li>
-    </ul>
-    <button @click="sendEvent">
-      Send Event
-    </button>
-  </div>
-</template>
-
 <script>
-export default {
-  methods: {
-    sendEvent () {
-      if (this.$config.ackeeTestEvent) {
-        this.$ackee.action(
-          this.$config.ackeeTestEvent,
-          { key: 'Test', value: 1 }
-        )
-      }
-    }
-  }
-}
+import _ from './index.vue'
+export default _
 </script>
